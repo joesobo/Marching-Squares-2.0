08b325789361c87568822f5b297f5f08dbe2016a
Joe <soboleskijoe@gmail.com>
2021-11-14 15:32:51 -0800

    slight reorganization with namespace

--- Assets/Scripts/Core/VoxelCore.cs
+++ Assets/Scripts/Core/VoxelCore.cs
@@ -2,44 +2,46 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoxelCore : MonoBehaviour {
-    // Number of voxels per a chunk 
-    public int voxelResolution = 8;
-    // Radius of chunks around the player
-    public int chunkResolution = 2;
-    // Whether or not to display the points where voxels are generated
-    public bool showVoxelReferencePoints = true;
-    // The element to spawn at each reference position along the chunk
-    public GameObject voxelReferencePointsPrefab;
+namespace Core {
+    public class VoxelCore : MonoBehaviour {
+        // Number of voxels per a chunk 
+        public int voxelResolution = 8;
+        // Radius of chunks around the player
+        public int chunkResolution = 2;
+        // Whether or not to display the points where voxels are generated
+        public bool showVoxelReferencePoints = true;
+        // The element to spawn at each reference position along the chunk
+        public GameObject voxelReferencePointsPrefab;
 
-    // Center point for chunk generation
-    private Vector2 playerPosition = Vector2.zero;
+        // Center point for chunk generation
+        private Vector2 playerPosition = Vector2.zero;
 
-    // List of chunks
-    private List<VoxelChunk> chunks;
+        // List of chunks
+        private List<VoxelChunk> chunks;
 
-    // Generators for building
-    private VoxelChunkGenerator chunkGenerator;
-    private VoxelMeshGenerator meshGenerator;
+        // Generators for building
+        private VoxelChunkGenerator chunkGenerator;
+        private VoxelMeshGenerator meshGenerator;
 
-    void Awake() {
-        chunkGenerator = FindObjectOfType<VoxelChunkGenerator>();
-        meshGenerator = FindObjectOfType<VoxelMeshGenerator>();
+        void Awake() {
+            chunkGenerator = FindObjectOfType<VoxelChunkGenerator>();
+            meshGenerator = FindObjectOfType<VoxelMeshGenerator>();
 
-        FreshGeneration();
-    }
+            FreshGeneration();
+        }
 
-    private void FreshGeneration() {
-        chunks = new List<VoxelChunk>();
+        private void FreshGeneration() {
+            chunks = new List<VoxelChunk>();
 
-        GenerateTerrain();
-    }
+            GenerateTerrain();
+        }
 
-    private void GenerateTerrain() {
-        chunks = chunkGenerator.SetupChunks(chunkResolution, voxelResolution, showVoxelReferencePoints);
-        chunkGenerator.CreateChunks(chunks);
+        private void GenerateTerrain() {
+            chunks = chunkGenerator.SetupChunks(chunkResolution, voxelResolution, showVoxelReferencePoints);
+            chunkGenerator.CreateChunks(chunks);
 
-        meshGenerator.Setup(voxelResolution, chunkResolution);
-        meshGenerator.GenerateMesh(chunks);
+            meshGenerator.Setup(voxelResolution, chunkResolution);
+            meshGenerator.GenerateMesh(chunks);
+        }
     }
 }
