ad895fcba4f4b87ad7be14d13afd4b8841ee537f
Joe <soboleskijoe@gmail.com>
2022-02-13 12:27:53 -0800

    feat: odin enums

--- Assets/ScriptableObjects/TerrainEditingScriptableObject.cs
+++ Assets/ScriptableObjects/TerrainEditingScriptableObject.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "CoreData", menuName = "ScriptableObjects/TerrainEditingScriptableObject", order = 2)]
 public class TerrainEditingScriptableObject : SerializedScriptableObject {
+    [EnumToggleButtons]
     public enum Stencil {
         Square,
         Circle
     }
 
+    [EnumToggleButtons]
     public enum Type {
         Remove,
         Dirt,
